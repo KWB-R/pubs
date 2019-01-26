bd5b3cd8c21485d6fef28f53eb71754e7ae3813f
mrustl <michael.rustler@kompetenz-wasser.de>
2019-01-26 11:11:23 +0100

    Update to https://github.com/gcushen/hugo-academic/commit/8a7c4eb2aade681b51499d5e8dd6d82cf05095c8

--- _deploy.sh
+++ _deploy.sh
@@ -3,12 +3,12 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "dev" ] && exit 0
 
 git config --global user.email "michael.rustler@kompetenz-wasser.de"
 git config --global user.name "Michael Rustler"
 
-git clone -b gh-pages \
+git clone -b master \
   https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
   website-output
 cd website-output
@@ -16,4 +16,4 @@ git rm -rf *
 cp -r ../public/* ./
 git add --all *
 git commit -m "update homepage (travis build ${TRAVIS_BUILD_NUMBER})" || true
-git push -q origin gh-pages
+git push -q origin master
