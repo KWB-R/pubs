bfad16f6173bd7192fccdaf4e6f9a8a348823f51
mrustl <michael.rustler@kompetenz-wasser.de>
2020-08-30 23:58:35 +0200

    algolia: integrate first working version

--- assets/js/algolia-search.js
+++ assets/js/algolia-search.js
@@ -1,4 +1,111 @@
-import searchRouting from './search-routing';
+const router = instantsearch.routers.history({
+  createURL({ qsModule, routeState, location }) {
+    const urlParts = location.href.match(/^(.*?)\/search/);
+    const baseUrl = `${urlParts ? urlParts[1] : ''}/`;
+
+    const queryParameters = {};
+
+    if (routeState.query) {
+      queryParameters.query = encodeURIComponent(routeState.query);
+    }
+    if (routeState.page !== 1) {
+      queryParameters.page = routeState.page;
+    }
+    if (routeState.type) {
+      queryParameters.type = routeState.type.map(encodeURIComponent);
+    }
+    if (routeState.year) {
+      queryParameters.year = routeState.year.map(encodeURIComponent);
+    }
+    if (routeState.author) {
+      queryParameters.author = routeState.author.map(encodeURIComponent);
+    }
+    if (routeState.project) {
+      queryParameters.project = routeState.project.map(encodeURIComponent);
+    }
+
+    const queryString = qsModule.stringify(queryParameters, {
+      addQueryPrefix: true,
+      arrayFormat: 'repeat',
+    });
+
+    return `${baseUrl}search${queryString}`;
+  },
+
+  parseURL({ qsModule, location }) {
+    const {
+      query = '',
+      page,
+      type = [],
+      year = [],
+      author = [],
+      project = [],
+    } = qsModule.parse(location.search.slice(1));
+    // `qs` does not return an array when there's a single value.
+    const allTypes = Array.isArray(type) ? type : [type].filter(Boolean);
+    const allYears = Array.isArray(year) ? year : [year].filter(Boolean);
+    const allAuthors = Array.isArray(author)
+      ? author
+      : [author].filter(Boolean);
+    const allProjects = Array.isArray(project)
+      ? project
+      : [project].filter(Boolean);
+
+    return {
+      query: decodeURIComponent(query),
+      page,
+      type: allTypes.map(decodeURIComponent),
+      year: allYears.map(decodeURIComponent),
+      author: allAuthors.map(decodeURIComponent),
+      project: allProjects.map(decodeURIComponent),
+    };
+  },
+});
+
+const stateMapping = {
+  stateToRoute(uiState) {
+    // refer to uiState docs for details: https://www.algolia.com/doc/api-reference/widgets/ui-state/js/
+    return {
+      query: uiState.pubs_test.query,
+      page: uiState.pubs_test.page,
+      type:
+        uiState.pubs_test.refinementList &&
+        uiState.pubs_test.refinementList.type,
+      year:
+        uiState.pubs_test.refinementList &&
+        uiState.pubs_test.refinementList.year,
+      author:
+        uiState.pubs_test.refinementList &&
+        uiState.pubs_test.refinementList.author,
+      project:
+        uiState.pubs_test.refinementList &&
+        uiState.pubs_test.refinementList.project,
+    };
+  },
+
+  routeToState(routeState) {
+    // refer to uiState docs for details: https://www.algolia.com/doc/api-reference/widgets/ui-state/js/
+    return {
+      // eslint-disable-next-line camelcase
+      pubs_test: {
+        query: routeState.query,
+        page: routeState.page,
+        refinementList: {
+          type: routeState.type,
+          year: routeState.year,
+          author: routeState.author,
+          project: routeState.project,
+        },
+      },
+    };
+  },
+};
+
+const searchRouting = {
+  router,
+  stateMapping,
+};
+
 
 /* global instantsearch algoliasearch */
 
@@ -69,7 +176,7 @@ search.addWidgets([
       <a role="button" href=".">Clear all filters</a>
     </div>`,
       item: function (data) {
-        const base_url = 'https://deploy-preview-47--kwb.netlify.app';
+        const base_url = '';
         const abstract_id = 'abstract-' + data.__hitIndex + 1;
         const authors = data._highlightResult.author
           .map((a) => '<a href="?author=' + a.value + '">' + a.value + '</a>')
@@ -90,19 +197,21 @@ search.addWidgets([
             .join('');
         }
         const cite =
-          '<a class="btn btn-outline-primary my-1 mr-1 btn-sm js-cite-modal" href="' +
+          '<button class="btn btn-outline-primary my-1 mr-1 btn-sm js-cite-modal" type="button" data-filename="' +
           base_url +
           data.cite_link +
           '">' +
           data.cite_name +
-          '</a>';
+          '</button>';
         let pdf = '';
         if (data.pdf !== '') {
           pdf +=
             '<a class="btn btn-outline-primary my-1 mr-1 btn-sm" href="' +
             base_url +
             data.pdf +
-            '">PDF</a>';
+            '">' +
+			'PDF' + 
+			'</a>';
         }
         let doi = '';
         if (data.doi !== null) {
@@ -114,14 +223,15 @@ search.addWidgets([
         }
         const links = '<p>' + cite + doi + pdf + project + '</p>';
         const publication =
-          '<div>' +
+          '<div class="pub-list-item" style="margin-bottom: 1rem">' +
+          '<i class="far fa-file-alt pub-icon" aria-hidden="true"></i>' +
           '<span class="article-metadata li-cite-author">' +
           authors +
           '</span>' +
           ' (' +
           data._highlightResult.year.value +
           '): <a href= ' +
-          data.permalink +
+          data.relpermalink +
           '> ' +
           data._highlightResult.title.value +
           '</a>. ' +
